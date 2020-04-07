830bafea4d291ff507cdcd4de3846f2dab4d0673
Jason <nicnac.brown@gmail.com>
2020-04-07 16:05:32 -0600

    Hello world

---
+++ src/main/java/Main.java
@@ -0,0 +1,32 @@
+import org.hexworks.zircon.api.CP437TilesetResources;
+import org.hexworks.zircon.api.ColorThemes;
+import org.hexworks.zircon.api.Components;
+import org.hexworks.zircon.api.SwingApplications;
+import org.hexworks.zircon.api.application.AppConfig;
+import org.hexworks.zircon.api.component.Label;
+import org.hexworks.zircon.api.data.Size;
+import org.hexworks.zircon.api.grid.TileGrid;
+import org.hexworks.zircon.api.screen.Screen;
+
+public class Main {
+    public static void main(String[] args) {
+        TileGrid tileGrid = SwingApplications.startTileGrid(
+                AppConfig.newBuilder()
+                .withSize(Size.create(80,24))
+                .withDefaultTileset(CP437TilesetResources.rexPaint16x16())
+                        .build());
+
+        Screen screen = Screen.create(tileGrid);
+
+        Label label = Components.label()
+                .withText("Hello world!")
+                .withPosition(23 ,10)
+                .build();
+
+        screen.addComponent(label);
+
+        screen.display();
+
+        screen.setTheme(ColorThemes.tron());
+    }
+}
